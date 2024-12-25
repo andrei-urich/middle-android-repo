447bfb4dd5db8c0599d7528705d411e52cd0c5b4
Andrei Urich <andrei.urich@yandex.ru>
2024-12-25 13:55:52 +0400

    Сделал главный экран

--- app/src/main/java/com/example/androidpracticumcustomview/MainActivity.kt
+++ app/src/main/java/com/example/androidpracticumcustomview/MainActivity.kt
@@ -1,45 +1,16 @@
 package com.example.androidpracticumcustomview
 
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.widget.TextView
 import androidx.activity.ComponentActivity
-import com.example.androidpracticumcustomview.ui.theme.CustomContainer
-
-/*
-Задание:
-Реализуйте необходимые компоненты.
-*/
+import androidx.activity.compose.setContent
+import com.example.androidpracticumcustomview.ui.theme.MainScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        /*
-        Раскомментируйте нужный вариант
-         */
-        startXmlPracticum() // «традиционный» android (XML)
-//          setContent { // Jetpack Compose
-//             MainScreen()
-    }
 
-    private fun startXmlPracticum() {
-        val customContainer = CustomContainer(this)
-        setContentView(customContainer)
-
-        val firstView = TextView(this).apply {
-            // TODO
-            // ...
+        setContent {
+            MainScreen()
         }
-
-        val secondView = TextView(this).apply {
-            // TODO
-            // ...
-        }
-
-        // Добавление второго элемента через некоторое время
-        Handler(Looper.getMainLooper()).postDelayed({
-            customContainer.addView(secondView)
-        }, 2000)
     }
 }
--- app/src/main/java/com/example/androidpracticumcustomview/ui/theme/MainScreen.kt
+++ app/src/main/java/com/example/androidpracticumcustomview/ui/theme/MainScreen.kt
@@ -1,36 +1,56 @@
 package com.example.androidpracticumcustomview.ui.theme
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
-/*
-Задание:
-Реализуйте необходимые компоненты.
-*/
-
+@Preview
 @Composable
 fun MainScreen() {
-    Scaffold { paddingValues ->
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(paddingValues), contentAlignment = Alignment.Center
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Button(
+            onClick = {},
+            colors = ButtonColors(
+                Color.Blue,
+                Color.White,
+                Color.Blue,
+                Color.White
+            ),
         ) {
-
-            CustomContainerCompose(
-                firstChild = {
-                    // TODO
-                    // ...
-                },
-                secondChild = {
-                    // TODO
-                    // ...
-                }
+            Text(
+                text = "Screen_1 CustomView on Views",
+                modifier = Modifier,
+                color = Color.White,
+                fontSize = 14.sp,
+            )
+        }
+        Spacer(
+            modifier = Modifier.height(40.dp)
+        )
+        Button(
+            onClick = {}
+        ) {
+            Text(
+                text = "Screen_2 CustomView on Compose",
+                modifier = Modifier,
+                color = Color.White,
+                fontSize = 14.sp,
             )
         }
     }
